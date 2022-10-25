d4a1a4b85bb13f0f6752ce6c7e35b755146cc7e2
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-25 16:43:04 +0200

    Props and prop drilling
    Worked with props to create better reusable components.

--- react01/src/App.js
+++ react01/src/App.js
@@ -1,13 +1,51 @@
 import Header from './Header';
 import Content from './Content';
-import Footer from './Footer'
+import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
+
+  const [items, setItmes] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'Cocoa covered almonds'
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'item 2'
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'item 3'
+    }
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
+    setItmes(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItmes(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
+
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Grocerie List"/>
+      <Content 
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+        />
+      <Footer length={items.length} />
     </div>
   );
 }
--- react01/src/Content.js
+++ react01/src/Content.js
@@ -1,55 +1,14 @@
-import { useState } from 'react';
+import ItemList from './ItemList';
 
-const Content = () => {
-  const [items, setItmes] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'Cocoa covered almonds'
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'item 2'
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'item 3'
-    }
-  ]);
-    
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
-    setItmes(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItmes(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
+const Content = ({items, handleCheck, handleDelete}) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-                />
-                <label 
-                  style={(item.checked) ? {textDecoration: 'line-through'} : null}
-                  onDoubleClick={() => handleCheck(item.id)}
-                >{item.item}</label>
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
+        <ItemList 
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p style={{ marginTop: '2rem' }}>Your list is empty</p>
       )}
