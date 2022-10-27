ac3ed6594c58a48bb5bda3a808202702795325be
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-27 16:02:04 +0200

    Creating component and added react router.

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,9 +1,27 @@
-
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Routes, useHistory} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={ <Home/> }></Route>
+        <Route exact path="/post" element={ <NewPost/> }></Route>
+        <Route exact path="/post/:id" element={ <PostPage/> }></Route>
+        <Route exact path="/about" element={ <About/> } />
+        <Route path="*" element={ <Missing/> } />
+      </Routes>
+      <Footer />
     </div>
   );
 }
