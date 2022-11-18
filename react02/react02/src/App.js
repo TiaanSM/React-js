60cb76c37f031108ad42556ce8626c61d129da2f
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-18 18:42:46 +0200

    Used intersection observer.
    Using framer, intersection observer I created a element that slides into view when the element is in view. Learning purpose & for project

--- react02/react02/src/App.js
+++ react02/react02/src/App.js
@@ -1,9 +1,31 @@
-import { motion } from 'framer-motion';
-import {useState} from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 function App() {
 
   const [move, setMove] = useState(false);
+
+  const { ref, inView } = useInView({  // Intersection observer api
+    threshold: 0.2  // 20% of element must be visible to trigger
+  });
+
+  const animation = useAnimation();  
+
+  useEffect(() => {
+    if(inView) {
+      animation.start({
+        x: 0,
+        transition: {
+          type: 'spring', duration: 1, bounce: 0.3
+        }
+      });
+    }
+    if(!inView) {
+      animation.start({ x: '-100vw' })
+    }
+    console.log("use effect, inView =", inView);
+  },[inView])
 
   // transition={{ delay: 0.5 }} => delays animation.
   // transition={{ type:"tween", duration: 5 }} => animationn duration
@@ -24,12 +46,17 @@ function App() {
   // repeat: 2, => repeats the animation based on given amount. Infinity to keep it going.
   
   return (
-    <div className="app">
 
-        <motion.div 
+    <div className="app" ref={ref}>
+
+        <motion.div className="div"
           animate={{ rotate: [0, 200, 200, 0] }}
           transition={{ repeat: 2, duration: 1 }}
         ></motion.div>
+
+        <motion.div className="div"
+          animate={animation}>   
+        </motion.div>
 
     </div>
   )
