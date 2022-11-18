5097d95e2d1526d48c51c7ea4b2191c90f58e613
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-11-18 15:08:39 +0200

    Framer motion intro.
    Completed a few basic animation with framer motion for learning purposes.

--- react02/react02/src/App.js
+++ react02/react02/src/App.js
@@ -1,22 +1,36 @@
-import Square from "./Square";
-import Input from "./Input";
-import { useState } from 'react';
+import { motion } from 'framer-motion';
+import {useState} from 'react';
 
 function App() {
-  const [colorValue, setColorValue] = useState('')
-  const [hexValue, setHexValue] = useState('')
 
+  const [move, setMove] = useState(false);
+
+  // transition={{ delay: 0.5 }} => delays animation.
+  // transition={{ type:"tween", duration: 5 }} => animationn duration
+  // transition={{ type:"spring", bounce: 100 }} => animation physics
+
+  /* EXAMPLE ANIMATION 
+    animate={{ x: move ? 200 : -200 }}  // animates on render
+    transition={{ type:"inertia", velocity: 40 }}  // animation properties
+    onClick={() => {setMove(!move)}}  // animation trigger
+  */
+
+  // whileHover={{ scale : 2 }} => "event" types
+  // whileDrag => with the drag added it allows to drag element. 
+  // drag="x" => drags element x direction, works with y. 
+
+  // for keyframe animation => animate={{ rotate: [0, 200, 200, 0] }} // use an array.
+
+  // repeat: 2, => repeats the animation based on given amount. Infinity to keep it going.
+  
   return (
-    <div classNAme="app">
-      <Square 
-        colorValue={colorValue}
-        hexValue={hexValue}
-        />
-      <Input 
-        colorValue={colorValue}
-        setColorValue={setColorValue}
-        setHexValue={setHexValue}
-      />
+    <div className="app">
+
+        <motion.div 
+          animate={{ rotate: [0, 200, 200, 0] }}
+          transition={{ repeat: 2, duration: 1 }}
+        ></motion.div>
+
     </div>
   )
 }
